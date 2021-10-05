6d82e504fe235092607b50c3b866317ca231214e
Gantry York <gantry.engineer@gmail.com>
2021-10-04 20:40:39 -0700

    Learning Rust

---
+++ LearnRust/helloworld.rs
@@ -0,0 +1,18 @@
+// This is the main function
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    println!("I'm a Rustacean");
+
+    let mut a_bool = true;
+    let mut a_float = 2.0;
+    println!("{}", a_bool);
+    println!("{}", a_float);
+    a_bool = false;
+    a_float = 3.14;
+    println!("{}", a_bool);
+    println!("{}", a_float);
+
+}
