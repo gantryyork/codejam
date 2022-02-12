380b05012df697339b8e1f27906448779b159550
Gantry York <gantry.engineer@gmail.com>
2022-02-12 09:41:47 -0700

    more changes

--- SubhadipRoy/parser.py
+++ SubhadipRoy/parser.py
@@ -2,12 +2,14 @@
 import re
 
 filename = "./input.txt"
+M_suffix = ['A', 'B', 'C']
 
 
 def main():
 
-    FILE = open(filename)
+    FILE = open(filename)  # Open filehandle for reading
 
+    # Parse all data
     parsed_data = list()
     for line in FILE:
 
@@ -15,11 +17,39 @@ def main():
 
         if data_search:
             data = data_search.group(1).split()
-            print(data)
             parsed_data.append(data)
 
-    parsed_data.sort()
-    print(parsed_data)
+    # Modify data
+    parsed_data.sort()  # sorts parsed_data by first col
+    last_pin = ''
+    M_suffix_index = 0
+
+    for data in parsed_data:
+
+        [pin, num1, num2, M] = data
+        #print(f"{pin} {num1} {num2} '{M}'")
+
+        # Adding M suffix if necessary
+
+        if pin == last_pin:
+            if M in ['M2', 'M3', 'M4']:
+                M = M + "_" + M_suffix[M_suffix_index]
+
+                # if you run out of M suffixes, start over
+                if M_suffix_index == len(M_suffix):
+                    M_suffix_index = 0
+                else:
+                    M_suffix_index += 1
+
+        else:
+            M_suffix_index = 0
+
+        last_pin = pin
+
+        # Add _probe to pin
+        pin = pin + "_probe"
+
+        print(f"{pin} {num1} {num2} {M}")
 
 
 if __name__ == "__main__":
