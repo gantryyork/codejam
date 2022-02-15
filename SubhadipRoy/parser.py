da7bf27bef873bbbdcc7d9e404cd1a04d3a5f922
Gantry York <gantry.engineer@gmail.com>
2022-02-14 17:33:55 -0700

    Final commit

--- SubhadipRoy/parser.py
+++ SubhadipRoy/parser.py
@@ -1,57 +1,97 @@
 #!/usr/bin/env python3
 import re
+import argparse
 
 filename = "./input.txt"
 M_suffix = ['A', 'B', 'C']
 
 
-def main():
+def main(args):
 
+    # Read in the file
+    print(f"Reading {args.inputfile}")
+    data = read_file(args.inputfile)
+
+    # Modify data per requirements
+    data = modify_data(data)
+
+    # Display data
+    display_data(data)
+
+
+def read_file(filename):
     FILE = open(filename)  # Open filehandle for reading
 
     # Parse all data
-    parsed_data = list()
+    input_data = list()
     for line in FILE:
 
         data_search = re.match('.*__(.*)', line)
 
         if data_search:
             data = data_search.group(1).split()
-            parsed_data.append(data)
+            input_data.append(data)
 
-    # Modify data
-    parsed_data.sort()  # sorts parsed_data by first col
-    last_pin = ''
+    return input_data
+
+
+def modify_data(data):
+
+    modified_data = list()
     M_suffix_index = 0
+    next_pin = ''
+    last_pin = ''
 
-    for data in parsed_data:
+    # Sort data.  Array of arrays is sorted on 1st columnn
+    data.sort()
 
-        [pin, num1, num2, M] = data
-        #print(f"{pin} {num1} {num2} '{M}'")
+    for i in range(0, len(data) - 1):
 
-        # Adding M suffix if necessary
+        [pin, coef1, coef2, M] = data[i]
 
-        if pin == last_pin:
-            if M in ['M2', 'M3', 'M4']:
-                M = M + "_" + M_suffix[M_suffix_index]
+        if i+1 != len(data):
+            next_pin = data[i+1][0]
 
-                # if you run out of M suffixes, start over
-                if M_suffix_index == len(M_suffix):
+        # modifying M
+        if M in ['M2', 'M3', 'M4']:
+
+            # If next pin is the same as current pin
+            if (pin == next_pin) or (pin == last_pin):
+                M = f"{M}_{M_suffix[M_suffix_index]}"
+
+                # Cycle to front of M_suffix list
+                if M_suffix_index == len(M_suffix) - 1:
                     M_suffix_index = 0
                 else:
                     M_suffix_index += 1
 
-        else:
-            M_suffix_index = 0
+            if pin != next_pin:
+                M_suffix_index = 0
 
+        # Modifying pin
         last_pin = pin
+        pin = f"{pin}_probe"
 
-        # Add _probe to pin
-        pin = pin + "_probe"
+        modified_data.append([pin, coef1, coef2, M])
 
-        print(f"{pin} {num1} {num2} {M}")
+    return modified_data
+
+
+def display_data(data):
+
+    for record in data:
+        print(f"{record[0]} {record[1]} {record[2]} {record[3]}")
 
 
 if __name__ == "__main__":
-    main()
-    exit()
+    parser = argparse.ArgumentParser(
+        description="Subhadip's parsing tool"
+    )
+    parser.add_argument(
+        '-i', '--inputfile',
+        metavar='FILENAME',
+        default="./input.txt",
+        help="Input file to parse"
+    )
+    args = parser.parse_args()
+    main(args)
